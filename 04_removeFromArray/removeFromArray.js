2ffc16f761326a299e80da5873f3d05a04db3e4f
Kris Chambers <krischambers86@pm.me>
2022-09-30 10:09:14 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    const removeLength = remove.length
+    const length = array.length;
+    let newArray = [];
+        for (let i = 0; i < length; i++) {
+            if (remove.includes(array[i])) {
+                continue;
+            } else {
+                newArray.push(array[i]);
+            }
+        } return newArray;
 };
 
 // Do not edit below this line

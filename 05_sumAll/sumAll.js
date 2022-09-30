37d5c8c77d31adc9045577ee9b22d367fc55efd7
Kris Chambers <krischambers86@pm.me>
2022-09-30 17:09:08 -0400

    Complete 04_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+let total = 0
+if (int1 < 0 || int2 < 0 || typeof int1 != "number" || typeof int2 != "number") {
+  return "ERROR"
+} else {  
+  if (int1 < int2) {
+    for (let i = int1; i < int2 + 1; i++) {
+      total = total + i;
+    }
+  } else {
+      for (let i = int2; i < int1 + 1; i++) {
+        total = total + i;
+      }
+  } return total;
+}
 };
 
 // Do not edit below this line

39a59ec27ab5fd0396849f10e8a711edcff00320
Kris Chambers <krischambers86@pm.me>
2022-09-30 19:59:52 -0400

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+let ctemp = ((5/9) * (ftemp - 32));
+return Math.round(ctemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+let ftemp = (((9/5) * ctemp) + 32);
+return Math.round(ftemp * 10) / 10;
 };
 
 // Do not edit below this line

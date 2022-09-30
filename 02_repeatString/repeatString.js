52092d8ae9359fb8e2581f6ebdac6c42069344b8
Kris Chambers <krischambers86@pm.me>
2022-09-29 21:19:40 -0400

    Start 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+  let newString = '';
+  while (num > 0) {
+    newString += string;
+    num--;
+  }
+  return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;

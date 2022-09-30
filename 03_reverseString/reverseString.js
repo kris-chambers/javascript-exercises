84c0f58cb24378ad825745d762d5445bff2dff5c
Kris Chambers <krischambers86@pm.me>
2022-09-29 23:12:40 -0400

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let stringArray = string.split("");
+let newArray = []
+const num = stringArray.length
+let newString = ''
+for (let i = 0; i < num;) {
+    let letter = stringArray.pop();
+    newArray.push(letter);
+    i++; 
+    };
+    newString = newArray.join('');
+    return newString;
 };
 
 // Do not edit below this line

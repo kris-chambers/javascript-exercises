861da774d73f97871fe4037ec7d7974cc52ce008
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2025-02-04 21:57:49 -0500

    Finished exercises
    exit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num >= 0) {
+    const array = [];
+    for (let i = 0; i < num; i++) {
+    array.push(string);
+    }
+    let answer = array.join('');
+    return answer
+  } else {
+    return 'ERROR';
+  }
+  
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const stringToArray = string.split("");
+  const reversedStringArray = [];
+  const length = stringToArray.length
+  for (let i = 0; i < length; i++) {
+    reversedStringArray.push(stringToArray.pop());
+  }
+  return reversedStringArray.join("");
 };
-
 // Do not edit below this line
 module.exports = reverseString;
